924e059eb4035b14f0d50659c0bd16acd6911ba4
Ritwick Dey <ritwickdey@outlook.com>
2017-10-16 10:56:18 +0530

    Initial Commit

---
+++ reload.js
@@ -0,0 +1,10 @@
+; (function () {
+    const protocol = window.location.protocol === 'http:' ? 'ws://' : 'wss://';
+    const address = protocol + window.location.host + window.location.pathname + '/ws';
+    const socket = new WebSocket(address);
+    socket.onmessage = function (msg) {
+        if (msg.data == 'reload' || msg.data == 'refreshcss')
+            window.location.reload();
+        console.log("reloaded. - From Extension");
+    };
+})();
