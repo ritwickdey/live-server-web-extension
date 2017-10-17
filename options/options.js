c6b223c8d4398d11e28940b64617a82ad8783f35
Ritwick Dey <ritwickdey@outlook.com>
2017-10-17 12:04:33 +0530

    options added

--- options/options.js
+++ options/options.js
@@ -1,9 +1,30 @@
 ; (function () {
+
     const liveReloadCheck = document.getElementById('liveReloadCheck');
     const SETUP_STRING = 'live-reload-extension-setup';
 
-    liveReloadCheck.onclick = () =>
-        localStorage.setItem(SETUP_STRING, liveReloadCheck.checked);
+    function sendMsg(status, val) {
+        chrome.tabs.query({}, tabs => {
+            tabs.forEach(tab => {
+                chrome.tabs.sendMessage(tab.id, {
+                    status: status,
+                    value: val
+                });
+            });
+        });
+    }
+    // chrome.runtime.onStartup = () => {
+    //     const val = localStorage.getItem(SETUP_STRING) === 'true';
+    //     liveReloadCheck.checked = val;
+    //     sendMsg('checkbox-status', val);
+    // };
 
-    liveReloadCheck.checked = localStorage.getItem(SETUP_STRING) === 'true';
+
+    liveReloadCheck.onclick = () => {
+        const val = liveReloadCheck.checked;
+        localStorage.setItem(SETUP_STRING, val);
+        sendMsg('checkbox-status', val);
+    }
+
+
 })();
--- reload.js
+++ reload.js
@@ -1,10 +1,18 @@
 ; (function () {
-    const protocol = window.location.protocol === 'http:' ? 'ws://' : 'wss://';
-    const address = protocol + window.location.host + window.location.pathname + '/ws';
-    const socket = new WebSocket(address);
-    socket.onmessage = function (msg) {
-        if (msg.data == 'reload' || msg.data == 'refreshcss')
-            window.location.reload();
-        console.log("reloaded. - From Extension");
-    };
+
+
+    chrome.runtime.onMessage.addListener((msg) => {
+        console.log(msg)
+    });
+
+
+    
+        const protocol = window.location.protocol === 'http:' ? 'ws://' : 'wss://';
+        const address = protocol + window.location.host + window.location.pathname + '/ws';
+        const socket = new WebSocket(address);
+        socket.onmessage = function (msg) {
+            if (msg.data == 'reload' || msg.data == 'refreshcss')
+                window.location.reload();
+            console.log("reloaded. - From Extension");
+        };
 })();
