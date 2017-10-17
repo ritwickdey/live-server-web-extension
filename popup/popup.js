732c208d6f342a2c45d369dc2a222df6f6aeaf6d
Ritwick Dey <ritwickdey@outlook.com>
2017-10-17 12:39:16 +0530

    cleanup

--- popup/popup.js
+++ popup/popup.js
@@ -1,29 +1,10 @@
 ; (function () {
 
     const liveReloadCheck = document.getElementById('liveReloadCheck');
-    // const SETUP_STRING = 'live-reload-extension-setup';
-
-    // function sendMsg(status, val) {
-    //     chrome.tabs.query({}, tabs => {
-    //         tabs.forEach(tab => {
-    //             chrome.tabs.sendMessage(tab.id, {
-    //                 status: status,
-    //                 value: val
-    //             });
-    //         });
-    //     });
-    // }
-    // chrome.runtime.onStartup = () => {
-    //     const val = localStorage.getItem(SETUP_STRING) === 'true';
-    //     liveReloadCheck.checked = val;
-    //     sendMsg('checkbox-status', val);
-    // };
 
 
     liveReloadCheck.onclick = () => {
         const val = liveReloadCheck.checked;
-       // localStorage.setItem(SETUP_STRING, val);
-        // sendMsg('checkbox-status', val);
         chrome.runtime.sendMessage({
             status: 'set-checkbox-status',
             value: val
--- reload.js
+++ reload.js
@@ -6,13 +6,13 @@
     });
 
 
-    
-        const protocol = window.location.protocol === 'http:' ? 'ws://' : 'wss://';
-        const address = protocol + window.location.host + window.location.pathname + '/ws';
-        const socket = new WebSocket(address);
-        socket.onmessage = function (msg) {
-            if (msg.data == 'reload' || msg.data == 'refreshcss')
-                window.location.reload();
-            console.log("reloaded. - From Extension");
-        };
+
+    const protocol = window.location.protocol === 'http:' ? 'ws://' : 'wss://';
+    const address = protocol + window.location.host + window.location.pathname + '/ws';
+    const socket = new WebSocket(address);
+    socket.onmessage = function (msg) {
+        if (msg.data == 'reload' || msg.data == 'refreshcss')
+            window.location.reload();
+        console.log("reloaded. - From Extension");
+    };
 })();
