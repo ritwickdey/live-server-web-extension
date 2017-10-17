a870bb3ba540181f6e95c8273df205cd03c5607f
Ritwick Dey <ritwickdey@outlook.com>
2017-10-17 13:01:31 +0530

    reload implemented

--- reload.js
+++ reload.js
@@ -1,18 +1,30 @@
 ; (function () {
-
-
-    chrome.runtime.onMessage.addListener((msg) => {
-        console.log(msg)
-    });
-
-
-
+    
     const protocol = window.location.protocol === 'http:' ? 'ws://' : 'wss://';
     const address = protocol + window.location.host + window.location.pathname + '/ws';
     const socket = new WebSocket(address);
-    socket.onmessage = function (msg) {
+    let isActive = false;
+
+    const reloadWindow = function (msg) {
+        if (!isActive) return;
+
         if (msg.data == 'reload' || msg.data == 'refreshcss')
             window.location.reload();
         console.log("reloaded. - From Extension");
     };
+
+    
+    chrome.runtime.onMessage.addListener((msg) => {
+        if (msg && msg.status && msg.status === 'checkbox-status-updated') {
+            isActive = msg.status;
+        }
+    });
+    
+    chrome.runtime.sendMessage({
+        status: 'get-checkbox-status'
+    }, (res) => {
+        isActive = res.value;
+        socket.onmessage = reloadWindow;
+    });
+    
 })();
