22f569a54ffe67482d75bd833d5dd418a1294529
Shafin <mahmood1807074@stud.kuet.ac.bd>
2022-04-27 12:47:24 +0600

    Java file added

---
+++ JavaFile.java
@@ -0,0 +1,27 @@
+public class JavaFile {
+        
+    int variable;
+    String works;
+    
+    public JavaFile(int v, String s) {
+        variable=v;
+        works=s;
+    }
+
+    public int getVariable() {
+        return variable;
+    }
+
+    public void setVariable(int variable) {
+        this.variable = variable;
+    }
+
+    public String getWorks() {
+        return works;
+    }
+
+    public void setWorks(String works) {
+        this.works = works;
+    }
+    
+}
